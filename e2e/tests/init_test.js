1e29a68dd35e11017f0b107de7a413ca1bc2b758
Didier Page <didier.page@heig-vd.ch>
2020-09-18 17:24:31 +0200

    Revert

--- e2e/tests/init_test.js
+++ e2e/tests/init_test.js
@@ -4,10 +4,3 @@ Scenario('test something', (I) => {
   I.amOnPage('https://github.com');
   I.see('GitHub');
 });
-
-Feature('My Second Test');
-
-Scenario('test something', (I) => {
-  I.amOnPage('https://github.com');
-  I.see('GitHub');
-});
